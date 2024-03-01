98f38963dd33e7ac513b08f3804b3a33f7e4fe46
Emad Aldeen Hammoude <ehammoud@lab3r6s5.42abudhabi.ae>
2024-03-01 14:39:45 +0400

    done

---
+++ inc/parser.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehammoud <ehammoud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 17:43:32 by ehammoud          #+#    #+#             */
+/*   Updated: 2024/03/01 13:18:32 by ehammoud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+# define UNDERSCORE '_'
+# define LOWERCASE "abcdefghijklmnopqrstuvwxyz"
+# define UPPERCASE "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# define DIGIT "0123456789"
+# define PIPE '|'
+# define DASH '-'
+# define AND '&'
+# define INF '<'
+# define OUF '>'
+# define FP '.'
+# define LP '('
+# define RP ')'
+# define DS '$'
+
+# include "general.h"
+
+#endif
